74ba5beee9e409fe0451206ee652a2ed7010fa31
otixis101 <preciouostis100@gmail.com>
2023-09-04 17:30:23 +0100

    faqs added

--- src/app/scholarships/containers/faqs.tsx
+++ src/app/scholarships/containers/faqs.tsx
@@ -18,96 +18,134 @@ const Faqs = () => {
                         <AccordionItem value="item-1 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">Who is INGRYD Academy?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                INGRYD Academy is a specialized training institution that offers comprehensive programs
+                                in various IT skills, including product management, DevOps, React, Cybersecurity, Python,
+                                Java, NodeJS, Linux, and IT solution sales. Our goal is to equip individuals with the
+                                necessary knowledge and skills to excel in the rapidly evolving IT industry.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-2 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">What are the available learning styles at INGRYD Academy?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                We offer two learning styles to accommodate diverse needs:
+                                Hybrid Learning: This style combines in-classroom and virtual classes. It includes 2 days
+                                of in-class lectures and 2 days of virtual classes per week, providing a balanced and
+                                interactive learning experience.
+                                Fully Virtual Learning: We also offer a remote experience for selected courses. You&apos;ll have
+                                access to all lectures and resources online, allowing flexibility while still benefiting from
+                                our comprehensive curriculum.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-3 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">How do I apply to INGRYD academy&apos;s programs</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                Applying to our programs is easy. Start by “Signing up”, once completed. Enroll in any of
+                                our courses you&apos;re interested in. Follow the prompts to complete your application.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-4 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">What happens after I complete the program?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                Upon successful completion of the program, you&apos;ll receive a certification that demonstrates
+                                your proficiency in your chosen IT field. Additionally, we place graduates in our partner
+                                companies you&apos;ll receive hands on experience to apply all that was learnt during training.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-5 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">What are the payment options available for the program?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                Applying to our programs is easy. Start by “Signing up”, once completed. Enroll in any of
+                                our courses you&apos;re interested in. Follow the prompts to complete your application.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-6 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">Are there any prerequisites for applying to the programs?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                The prerequisites for joining the INGRYD Academy vary depending on the program.
+                                However, the following are standard requirements to join the INGRYD Academy.
+                                - Must be between the age of 18 - 35
+                                - Must be a graduate in computer sciences, business, and related courses.
+                                - Has completed youth service or currently serving (with some exceptions)
+                                - Be available for training for the entire duration of the training.
+                                - Must be willing to wait a three-month period after training for job placements
+                                - Must be willing to sign a post-dated cheque with full training fee.
+                                - Must be willing to attend the additional job fitness training and mentorship
+                                - Be of proven integrity, with sensitivity to confidentiality requirements.
+                                - Be a team player and self-motivated.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-7 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">What happens if I cannot pay for the training?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                You can join our Scholarship Program, take the training for free and make payment after
+                                getting placed. To ensure qualification, applicants will be required to take and pass a pre
+                                assessment test to before gaining admission to the Scholarship Program.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-8 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">How can I get more information about a specific program?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                To learn more about a particular program, visit our website&apos;s &quot;Courses&quot; section. Each
+                                program has a dedicated page with detailed information about its curriculum, price, and
+                                learning outcomes.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-9 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">How does the assessment process work?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                Before enrolling in a course, all applicants are required to take an assessment related to
+                                their selected program. This helps us understand your current knowledge level and tailor
+                                the program to your needs. The assessment is designed to evaluate your familiarity with
+                                the course content.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-10 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">Can I switch programs after enrollment?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                While we encourage you to choose a program that aligns with your goals, we understand
+                                that circumstances may change. If you wish to switch programs, please contact our
+                                admissions team, and they will assist you with the necessary steps.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-11 border-t border-gray-400">
-                            <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">What is the duration of the program?</AccordionTrigger>
+                            <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">Are the hands-on-experience collaborative or individual?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                Our hands-on-experience encourage collaboration. During your industry experience, you&apos;ll
+                                have the opportunity to work in teams, to solve challenges, share insights, and learn from
+                                each other&apos;s experiences.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-12 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">Will I receive a certification upon completing the program?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                Yes, upon successful completion of the program requirements, you will receive a
+                                comprehensive certification from INGRYD Academy. This certification showcases your
+                                proficiency and skills in the selected program.
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-13 border-t border-gray-400">
                             <AccordionTrigger className="hover:no-underline text-xl lg:text-2xl text-left">What kind of support is available after graduation?</AccordionTrigger>
                             <AccordionContent className="text-lg">
-                                Answer Here.
+                                We offer ongoing support even after graduation. This includes access to our alumni
+                                network, mentorship, career counseling, and updates on industry trends. We are
+                                committed to helping you achieve your career goals.
                             </AccordionContent>
                         </AccordionItem>
 
 
-                        
+
                     </Accordion>
                 </div>
             </article>
