518c28c19a16a6614eca7ff14f82a9dba3a63e0c
otixis101 <preciouostis100@gmail.com>
2023-09-06 19:38:40 +0100

    slider fix

--- src/app/scholarships/containers/courseHighllight.tsx
+++ src/app/scholarships/containers/courseHighllight.tsx
@@ -76,26 +76,30 @@ const CourseHighllight = () => {
                         spaceBetween: 10,
                     },
                     768: {
-                        slidesPerView: 3,
+                        slidesPerView: 2,
                         spaceBetween: 10,
                     },
                     1024: {
                         slidesPerView: 3,
                         spaceBetween: 10,
                     },
+                    1280: {
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                    },
                 }}
             // navigation
             >
                 <SwiperSlide className='cursor-grab '>
-                    <li className="flex flex-col w-full aspect-[9/13] text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
+                    <li className="flex flex-col w-full text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
                         <div className='w-full shrink-0 bg-gray-100 h-32 relative'>
                             <Image src={ch3} alt="course image" className='object-cover' fill />
                         </div>
                         <div className='flex flex-col justify-between gap-2 grow p-2'>
                             <div className='flex justify-between p-2 border-b border-gray-100'>
-                                <p className='text-2xl font-semibold'>{'Cybersecurity '}</p>
+                                <p className='text-lg lg:text-xl font-semibold'>{'Cybersecurity '}</p>
                             </div>
-                            <p className='p-2 text-left'>
+                            <p className='overflow-clip line-clamp-5 text-ellipsis px-2 text-xs lg:text-sm text-left'>
                                 This 12-week Cybersecurity Course provides comprehensive training in protecting computer systems and networks from unauthorized access, use, disclosure, disruption, modification, or destruction...
                             </p>
                             <div className='flex flex-col gap-4 mt-auto'>
@@ -120,15 +124,15 @@ const CourseHighllight = () => {
                 </SwiperSlide>
                 {/* card2 */}
                 <SwiperSlide className='cursor-grab '>
-                    <li className="flex flex-col w-full aspect-[9/13] text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
+                    <li className="flex flex-col w-full text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
                         <div className='w-full shrink-0 bg-gray-100 h-32 relative'>
                             <Image src={ch2} alt="course image" className='object-cover' fill />
                         </div>
                         <div className='flex flex-col justify-between gap-2 grow p-2'>
                             <div className='flex justify-between p-2 border-b border-gray-100'>
-                                <p className='text-2xl font-semibold'>{'Linux '}</p>
+                                <p className='text-lg lg:text-xl font-semibold'>{'Linux '}</p>
                             </div>
-                            <p className='p-2 text-left'>
+                            <p className='overflow-clip line-clamp-5 text-ellipsis px-2 text-xs lg:text-sm text-left'>
                                 Discover the power of Linux, the most popular operating system used in servers, supercomputers, and embedded devices. Learn how to install and configure Linux distributions, manage files and permissions...
                             </p>
                             <div className='flex flex-col gap-4 mt-auto'>
@@ -152,15 +156,15 @@ const CourseHighllight = () => {
                     </li>
                 </SwiperSlide>
                 <SwiperSlide className='cursor-grab '>
-                    <li className="flex flex-col w-full aspect-[9/13] text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
+                    <li className="flex flex-col w-full text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
                         <div className='w-full shrink-0 bg-gray-100 h-32 relative'>
                             <Image src={ch1} alt="course image" className='object-cover' fill />
                         </div>
                         <div className='flex flex-col justify-between gap-2 grow p-2'>
                             <div className='flex justify-between p-2 border-b border-gray-100'>
-                                <p className='text-2xl font-semibold'>{'Java '}</p>
+                                <p className='text-lg lg:text-xl font-semibold'>{'Java '}</p>
                             </div>
-                            <p className='p-2 text-left'>
+                            <p className='overflow-clip line-clamp-5 text-ellipsis px-2 text-xs lg:text-sm text-left'>
                                 In this intensive Java programming course, you&apos;ll learn the fundamentals of object-oriented programming and master the Java syntax. From variables and loops to classes and objects, you&apos;ll gain a deep understanding of the Java...
                             </p>
                             <div className='flex flex-col gap-4 mt-auto'>
@@ -184,15 +188,15 @@ const CourseHighllight = () => {
                     </li>
                 </SwiperSlide>
                 <SwiperSlide className='cursor-grab '>
-                    <li className="flex flex-col w-full aspect-[9/13] text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
+                    <li className="flex flex-col w-full text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
                         <div className='w-full shrink-0 bg-gray-100 h-32 relative'>
                             <Image src={ch4} alt="course image" className='object-cover' fill />
                         </div>
                         <div className='flex flex-col justify-between gap-2 grow p-2'>
                             <div className='flex justify-between p-2 border-b border-gray-100'>
-                                <p className='text-2xl font-semibold'>{'Python for Data Science '}</p>
+                                <p className='text-lg lg:text-xl font-semibold'>{'Python for Data Science '}</p>
                             </div>
-                            <p className='p-2 text-left'>
+                            <p className='overflow-clip line-clamp-5 text-ellipsis px-2 text-xs lg:text-sm text-left'>
                                 In this comprehensive Python programming course, you&apos;ll dive into the world of object-oriented programming and become proficient in the Python syntax. Starting with the basics, you&apos;ll learn about variables, data types...
                             </p>
                             <div className='flex flex-col gap-4 mt-auto'>
@@ -217,15 +221,15 @@ const CourseHighllight = () => {
                 </SwiperSlide>
 
                 <SwiperSlide className='cursor-grab '>
-                    <li className="flex flex-col w-full aspect-[9/13] text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
+                    <li className="flex flex-col w-full text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
                         <div className='w-full shrink-0 bg-gray-100 h-32 relative'>
                             <Image src={ch5} alt="course image" className='object-cover' fill />
                         </div>
                         <div className='flex flex-col justify-between gap-2 grow p-2'>
                             <div className='flex justify-between p-2 border-b border-gray-100'>
-                                <p className='text-2xl font-semibold'>{'ReactJS '}</p>
+                                <p className='text-lg lg:text-xl font-semibold'>{'ReactJS '}</p>
                             </div>
-                            <p className='p-2 text-left'>
+                            <p className='overflow-clip line-clamp-5 text-ellipsis px-2 text-xs lg:text-sm text-left'>
                                 Get ready to take your web development skills to the next level with our comprehensive React course! Dive into the world of component-based architecture and learn how to build fast, scalable, and maintainable applications with React...
                             </p>
                             <div className='flex flex-col gap-4 mt-auto'>
@@ -249,15 +253,15 @@ const CourseHighllight = () => {
                     </li>
                 </SwiperSlide>
                 <SwiperSlide className='cursor-grab '>
-                    <li className="flex flex-col w-full aspect-[9/13] text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
+                    <li className="flex flex-col w-full text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
                         <div className='w-full shrink-0 bg-gray-100 h-32 relative'>
                             <Image src={ch6} alt="course image" className='object-cover' fill />
                         </div>
                         <div className='flex flex-col justify-between gap-2 grow p-2'>
                             <div className='flex justify-between p-2 border-b border-gray-100'>
-                                <p className='text-2xl font-semibold'>{'Product Management '}</p>
+                                <p className='text-lg lg:text-xl font-semibold'>{'Product Management '}</p>
                             </div>
-                            <p className='p-2 text-left'>
+                            <p className='overflow-clip line-clamp-5 text-ellipsis px-2 text-xs lg:text-sm text-left'>
                                 In this immersive product management course, you&apos;ll embark on a journey to master the art of managing products from ideation to launch. Starting with the foundational principles of product development...
                             </p>
                             <div className='flex flex-col gap-4 mt-auto'>
@@ -281,15 +285,15 @@ const CourseHighllight = () => {
                     </li>
                 </SwiperSlide>
                 <SwiperSlide className='cursor-grab '>
-                    <li className="flex flex-col w-full aspect-[9/13] text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
+                    <li className="flex flex-col w-full text-left select-none border border-gray-100 rounded-lg overflow-hidden shadow-lg">
                         <div className='w-full shrink-0 bg-gray-100 h-32 relative'>
                             <Image src={ch7} alt="course image" className='object-cover' fill />
                         </div>
                         <div className='flex flex-col justify-between gap-2 grow p-2'>
                             <div className='flex justify-between p-2 border-b border-gray-100'>
-                                <p className='text-2xl font-semibold'>{'IT Solution Sales '}</p>
+                                <p className='text-lg lg:text-xl font-semibold'>{'IT Solution Sales '}</p>
                             </div>
-                            <p className='p-2 text-left'>
+                            <p className='overflow-clip line-clamp-5 text-ellipsis px-2 text-xs lg:text-sm text-left'>
                                 Transform your sales game and close more deals with our elite IT solution sales training course! Designed for ambitious sales professionals like you, this comprehensive program covers everything from the fundamentals of IT...
                             </p>
                             <div className='flex flex-col gap-4 mt-auto'>
