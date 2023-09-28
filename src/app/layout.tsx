2293e1b00d4c9106f79544d203c75aeaa937d44e
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2023-09-28 09:21:53 -0700

    Added title to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Acquire Borderless Tech Skills & Get Job Placements | Ingryd',
-  description: 'Acquire Borderless Tech Skills & Get Job Placements',
-}
 
 export default function RootLayout({
   children,
