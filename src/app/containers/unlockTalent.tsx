08fe6a17b63a60293e7cdab6105e9cc7a7c65689
Jaybee <jubril_tunde@yahoo.com>
2023-11-02 20:46:17 +0100

    contents correction

--- src/app/containers/unlockTalent.tsx
+++ src/app/containers/unlockTalent.tsx
@@ -5,7 +5,7 @@ const UnlockTalent = () => {
     return (
         <section className="py-10 px-8 flex flex-col items-center text-center gap-4">
             <h1 className="text-2xl lg:text-5xl font-bold">
-                Ignite <span className="text-gray-500"> Your </span> Tech Journey
+                Boost <span className="text-gray-500"> Your </span> Tech Journey
             </h1>                       
             <p className='text-sm lg:text-xl text-center lg:px-52'>Welcome to INGRYD Academy, your premier tech talent partner in Africa. We're here to bridge the gap between recent tech graduates and the evolving needs of today's dynamic workforce.</p>
 
