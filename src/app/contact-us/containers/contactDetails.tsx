8f3efd45d421ddbbd491970a164831e337a5e156
Cornelius Samuel Ezeh <cornzeh@gmail.com>
2023-11-06 11:59:39 -0800

    Implemented the Contact us page

---
+++ src/app/contact-us/containers/contactDetails.tsx
@@ -0,0 +1,118 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const ContactDetails = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleTextAreaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  return (
+    <section className="py-16 lg:px-12 grid lg:grid-cols-5 overflow-x-hidden">
+      <div className="border-r border-gray-300 col-span-2 px-10">
+        <div className="mb-10">
+          <p className="lg:text-2xl text-lg  font-semibold">ADDRESS</p>
+          <p className="lg:text-xl text-base font-medium text-[#7A7A7A] mt-4">
+            4A Akiogun street New Market Road, Oniru Lagos State
+          </p>
+        </div>
+
+        <div className="mb-10">
+          <p className="lg:text-2xl text-lg font-semibold">PHONE</p>
+          <p className="lg:text-xl text-base font-medium text-[#7A7A7A] mt-4">
+            +234 (0)81 6588 3197
+          </p>
+        </div>
+
+        <div className="mb-10">
+          <p className="lg:text-2xl text-lg font-semibold">EMAIL</p>
+          <p className="lg:text-xl text-base font-medium text-[#7A7A7A] mt-4">
+            support@ingrydacademy.com
+          </p>
+        </div>
+
+        <div className="mb-10">
+          <p className="lg:text-2xl text-lg font-semibold">SOCIAL</p>
+          <p className="text-xl font-medium text-[#7A7A7A] mt-4">Icons</p>
+        </div>
+      </div>
+
+      <div className="col-span-3 px-10">
+        <form className="overflow-x-hidden">
+          <div className="flex flex-col lg:flex-row gap-10 mb-10">
+            <div>
+              <label htmlFor="" className="text-base font-medium">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                onChange={handleChange}
+                value={formValues.name}
+                className="border border-gray-300 block w-full lg:w-[315px] p-2 rounded text-base mt-1"
+              />
+            </div>
+            <div>
+              <label htmlFor="" className="text-base font-medium">
+                Email
+              </label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                onChange={handleChange}
+                value={formValues.email}
+                className="border border-gray-300 block w-full lg:w-[315px] p-2 rounded text-base mt-1"
+              />
+            </div>
+          </div>
+          <label htmlFor="" className="text-base font-medium">
+            Message
+          </label>
+          <textarea
+            className="w-full h-[200px] border border-gray-300 rounded p-2 mt-1"
+            name="message"
+            id="message"
+            onChange={handleTextAreaChange}
+            value={formValues.message}
+            style={{ resize: "none" }}
+          ></textarea>
+
+          <Button asChild className="bg-primary lg:w-1/3 w-full h-auto mt-6">
+            <Link
+              href={`${baseUrl}/signup`}
+              className="py-2 px-16 text-sm lg:text-xl font-semibold"
+            >
+              SEND EMAIL
+            </Link>
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ContactDetails;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,102 +1,119 @@
-import React from 'react'
-import imgLogo from '@/assets/logo.png'
-import Image from 'next/image'
-import Link from 'next/link'
-import Ig from "@/assets/Ig.png"
-import Twitter from "@/assets/Twitter.png"
-import Facebook from "@/assets/facebook.png"
-import Linkedin from "@/assets/in.png"
+import React from "react";
+import imgLogo from "@/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import Ig from "@/assets/Ig.png";
+import Twitter from "@/assets/Twitter.png";
+import Facebook from "@/assets/facebook.png";
+import Linkedin from "@/assets/in.png";
 
 const Footer = () => {
-    return (
-        <footer className=" bg-[#1A183E] flex flex-col text-gray-200">
-            
-            <div className=" gap-8 container lg:px-30 lg:pt-20 pt-10 flex flex-col lg:flex-row justify-between flex-wrap w-full">
-                <div className="flex flex-col gap-3 flex-1 lg:mr-32">
-                    <Image src={imgLogo} alt='logo' className='w-40' />
-                    <p>4A Akiogun Street, New Market Road, Oniru, Lagos State.</p>
-                    <p >08165883197</p>
-                    <p className="text-primary underline underline-offset-4"><Link href="https://support@ingrydacademy.com">support@ingrydacademy.com</Link></p>
-                </div>
-                <div className="flex flex-col flex-1 gap-5">
-                    <p className="text-lg font-semibold">Quick Links</p>
-                    <nav className="list-none flex flex-col gap-3">
-                        <li>
-                            <Link href={'/about-us'} className=" hover:text-primary">About us</Link>
-                        </li>
-                        <li>
-                            <Link href={'/request-staffing'} className=" hover:text-primary">Request Staffing</Link>
-                        </li>
-                        <li>
-                            <Link href={'/request-training'} className=" hover:text-primary">Request Training</Link>
-                        </li>
-                        <li>
-                            <Link href={'/for-training'} className=" hover:text-primary">Get Trained</Link>
-                        </li>
-                        <li>
-                            <Link href={'/find-jobs'} className=" hover:text-primary">Get Job Placement</Link>
-                        </li>
-                    </nav>
-                </div>
-                <div className="flex flex-col flex-1 gap-5">
-                    <p className="text-lg font-semibold">Resources</p>
-                    <nav className="list-none flex flex-col gap-3">
-                        <li>
-                            <Link href={'/scholarships'} className=" hover:text-primary">Scholarships</Link>
-                        </li>
-                        <li>
-                            <Link href={'#'} className=" hover:text-primary">Contact us</Link>
-                        </li>
-                        <li>
-                            <Link href={'#'} className=" hover:text-primary">FAQS</Link>
-                        </li>
-                    </nav>
-                </div>
-                <div className="flex flex-col flex-1 gap-5">
-                    <p className="text-lg font-semibold">Legals</p>
-                    <nav className="list-none flex flex-col gap-3">
-                        <li>
-                            <Link href={'/privacy'} className=" hover:text-primary">Privacy</Link>
-                        </li>
-                        <li>
-                            <Link href={'/conditions'} className=" hover:text-primary">Terms and Conditions</Link>
-                        </li>
-                    </nav>
-                </div>
-            </div>
-            <div className='flex items-center justify-center my-5 lg:justify-start lg:my-2 gap-3 lg:pl-30 container'>
-                        <a href="https://www.instagram.com/ingrydacademy/" target='_blank'>
-                            <Image
-                                src={Ig}
-                                alt='instagram icon'
-                            />
-                        </a>
-                        <a href="https://www.facebook.com/profile.php?id=100094777950769" target="_blank">
-                            <Image
-                                src={Facebook}
-                                alt='facebook icon'
-                            />
-                        </a>
-                        <a href="https://twitter.com/INGRYDAcademy" target='_blank'>
-                            <Image
-                                src={Twitter}
-                                alt='twitter icon'
-                            />
-                        </a>
-                        <a href="https://www.linkedin.com/company/ingrydacademy/" target='_blank'>
-                            <Image
-                                src={Linkedin}
-                                alt='linkedin icon'
-                            />
-                        </a>
-                    </div>
-            <div className="pb-8 lg:mt-10">
-                <div className="container text-center">
-                    <p>Copyright 2023 Ingryd | All Rights Reserved</p>
-                </div>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className=" bg-[#1A183E] flex flex-col text-gray-200">
+      <div className=" gap-8 container lg:px-30 lg:pt-20 pt-10 flex flex-col lg:flex-row justify-between flex-wrap w-full">
+        <div className="flex flex-col gap-3 flex-1 lg:mr-32">
+          <Image src={imgLogo} alt="logo" className="w-40" />
+          <p>4A Akiogun Street, New Market Road, Oniru, Lagos State.</p>
+          <p>08165883197</p>
+          <p className="text-primary underline underline-offset-4">
+            <Link href="https://support@ingrydacademy.com">
+              support@ingrydacademy.com
+            </Link>
+          </p>
+        </div>
+        <div className="flex flex-col flex-1 gap-5">
+          <p className="text-lg font-semibold">Quick Links</p>
+          <nav className="list-none flex flex-col gap-3">
+            <li>
+              <Link href={"/about-us"} className=" hover:text-primary">
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link href={"/request-staffing"} className=" hover:text-primary">
+                Request Staffing
+              </Link>
+            </li>
+            <li>
+              <Link href={"/request-training"} className=" hover:text-primary">
+                Request Training
+              </Link>
+            </li>
+            <li>
+              <Link href={"/for-training"} className=" hover:text-primary">
+                Get Trained
+              </Link>
+            </li>
+            <li>
+              <Link href={"/find-jobs"} className=" hover:text-primary">
+                Get Job Placement
+              </Link>
+            </li>
+          </nav>
+        </div>
+        <div className="flex flex-col flex-1 gap-5">
+          <p className="text-lg font-semibold">Resources</p>
+          <nav className="list-none flex flex-col gap-3">
+            <li>
+              <Link href={"/scholarships"} className=" hover:text-primary">
+                Scholarships
+              </Link>
+            </li>
+            <li>
+              <Link href={"/contact-us"} className=" hover:text-primary">
+                Contact us
+              </Link>
+            </li>
+            <li>
+              <Link href={"#"} className=" hover:text-primary">
+                FAQS
+              </Link>
+            </li>
+          </nav>
+        </div>
+        <div className="flex flex-col flex-1 gap-5">
+          <p className="text-lg font-semibold">Legals</p>
+          <nav className="list-none flex flex-col gap-3">
+            <li>
+              <Link href={"/privacy"} className=" hover:text-primary">
+                Privacy
+              </Link>
+            </li>
+            <li>
+              <Link href={"/conditions"} className=" hover:text-primary">
+                Terms and Conditions
+              </Link>
+            </li>
+          </nav>
+        </div>
+      </div>
+      <div className="flex items-center justify-center my-5 lg:justify-start lg:my-2 gap-3 lg:pl-30 container">
+        <a href="https://www.instagram.com/ingrydacademy/" target="_blank">
+          <Image src={Ig} alt="instagram icon" />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100094777950769"
+          target="_blank"
+        >
+          <Image src={Facebook} alt="facebook icon" />
+        </a>
+        <a href="https://twitter.com/INGRYDAcademy" target="_blank">
+          <Image src={Twitter} alt="twitter icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/ingrydacademy/"
+          target="_blank"
+        >
+          <Image src={Linkedin} alt="linkedin icon" />
+        </a>
+      </div>
+      <div className="pb-8 lg:mt-10">
+        <div className="container text-center">
+          <p>Copyright 2023 Ingryd | All Rights Reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
