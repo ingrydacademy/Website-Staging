e2524c5414a57288d350f24659f145447a209291
Cornelius Samuel Ezeh <cornzeh@gmail.com>
2023-11-07 07:38:22 -0800

    Implemented the social media handles

--- src/app/contact-us/containers/contactDetails.tsx
+++ src/app/contact-us/containers/contactDetails.tsx
@@ -2,6 +2,11 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React, { useState } from "react";
+import instagram from "@/assets/instagram-icon1.png";
+import facebook from "@/assets/facebook-icon1.png";
+import twitter from "@/assets/twitter-icon1.png";
+import linkedin from "@/assets/linkedin-icon1.png";
+import Image from "next/image";
 
 const ContactDetails = () => {
   const [formValues, setFormValues] = useState({
@@ -31,7 +36,7 @@ const ContactDetails = () => {
 
   return (
     <section className="py-16 lg:px-12 grid lg:grid-cols-5 overflow-x-hidden">
-      <div className="border-r border-gray-300 col-span-2 px-10">
+      <div className="lg:border-r border-b lg:border-b-0 mb-10 lg:mb-0 border-gray-300 col-span-2 px-10">
         <div className="mb-10">
           <p className="lg:text-2xl text-lg  font-semibold">ADDRESS</p>
           <p className="lg:text-xl text-base font-medium text-[#7A7A7A] mt-4">
@@ -55,7 +60,53 @@ const ContactDetails = () => {
 
         <div className="mb-10">
           <p className="lg:text-2xl text-lg font-semibold">SOCIAL</p>
-          <p className="text-xl font-medium text-[#7A7A7A] mt-4">Icons</p>
+          <div className="flex gap-10 mt-5">
+            <div className="md:w-[40px] md:h-[40px] w-[32px] h-[32px] rounded-full bg-[#FF00F8] flex justify-center items-center">
+              <Link
+                href="https://www.instagram.com/ingrydacademy/"
+                target="_blank"
+              >
+                <Image
+                  src={instagram}
+                  alt="instagram icon"
+                  className="object-cover inset-0 z-10 md:w-[24px] md:h-[24px] w-[16px] h-[16px]"
+                />
+              </Link>
+            </div>
+            <div className="md:w-[40px] md:h-[40px] w-[32px] h-[32px] rounded-full bg-[#FF00F8] flex justify-center items-center">
+              <Link
+                href="https://web.facebook.com/profile.php?id=100094777950769"
+                target="_blank"
+              >
+                <Image
+                  src={facebook}
+                  alt="instagram icon"
+                  className="object-cover inset-0 z-10 md:w-[24px] md:h-[24px] w-[16px] h-[16px]"
+                />
+              </Link>
+            </div>
+            <div className="md:w-[40px] md:h-[40px] w-[32px] h-[32px] rounded-full bg-[#FF00F8] flex justify-center items-center">
+              <Link href="https://twitter.com/INGRYDAcademy" target="_blank">
+                <Image
+                  src={twitter}
+                  alt="instagram icon"
+                  className="object-cover inset-0 z-10 md:w-[24px] md:h-[24px] w-[16px] h-[16px]"
+                />
+              </Link>
+            </div>
+            <div className="md:w-[40px] md:h-[40px] w-[32px] h-[32px] rounded-full bg-[#FF00F8] flex justify-center items-center">
+              <Link
+                href="https://www.linkedin.com/company/ingrydacademy/"
+                target="_blank"
+              >
+                <Image
+                  src={linkedin}
+                  alt="instagram icon"
+                  className="object-cover inset-0 z-10 md:w-[24px] md:h-[24px] w-[16px] h-[16px]"
+                />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -96,6 +147,7 @@ const ContactDetails = () => {
             className="w-full h-[200px] border border-gray-300 rounded p-2 mt-1"
             name="message"
             id="message"
+            placeholder="Add a message"
             onChange={handleTextAreaChange}
             value={formValues.message}
             style={{ resize: "none" }}
